0a4072c9990cb562334631af60c0433783a79b5f
Vishwa SSKM <vishwa180@gmail.com>
2021-08-23 00:27:14 +0530

    favicon added

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar() {
 	return (
 		<nav className='navbar navbar-dark bg-dark'>
 			<div className='container-fluid'>
-				<span className='navbar-brand mb-0 h1'>Hackernews</span>
+				<span className='navbar-brand mb-0 h1'>Hacker News</span>
 			</div>
 		</nav>
 	);
